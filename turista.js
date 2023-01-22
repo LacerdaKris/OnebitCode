bb1b6790a770e327f2869cd3cd01cee3cada4eb6
LacerdaKris <107269748+LacerdaKris@users.noreply.github.com>
2023-01-22 16:37:05 -0300

    Update turista.js

--- turista.js
+++ turista.js
@@ -1,3 +1,5 @@
+//resolução do exercício de cidades visitadas usando array/lista:
+
 const turista = prompt("Insira o nome do turista:");
 let visitou = prompt("Visitou alguma cidade? (sim/não)").toUpperCase();
 let cidades = [];
@@ -12,6 +14,28 @@ alert(
   turista +
     "visitou um total de " +
     cidades.length +
+    " cidades.\nSão elas: " +
+    cidades
+);
+
+//ou com concatenação de string e contador no while:
+
+const turista = prompt("Insira o nome do turista:");
+let visitou = prompt("Visitou alguma cidade? (sim/não)").toUpperCase();
+let cidades = "";
+let contagem = 0;
+
+while (visitou === "SIM") {
+  let cidade = prompt("Qual o nome da cidade?");
+  cidades += " - " + cidade + "\n";
+  contagem++;
+  visitou = prompt("Visitou mais alguma cidade? (sim/não)").toUpperCase();
+}
+
+alert(
+  turista +
+    "visitou um total de " +
+    contagem +
     " cidades.\nSão elas: " +
     cidades
 );
