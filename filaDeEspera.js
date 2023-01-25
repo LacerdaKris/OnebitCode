9e33fd73cfb9852f8fea6e1cd0252b0ebbb9e332
LacerdaKris <107269748+LacerdaKris@users.noreply.github.com>
2023-01-25 16:45:30 -0300

    Create filaDeEspera.js

---
+++ filaDeEspera.js
@@ -0,0 +1,34 @@
+// Fila de pacientes aguardando em consultório médico
+
+let opções = "";
+let pacientes = [];
+
+do {
+  let lista = [];
+
+  for (let i = 0; i < pacientes.length; i++) {
+    let j = i + 1;
+    lista.push(j + "º " + pacientes[i]);
+  }
+
+  opções = prompt(
+    lista.join([(separador = "\n")]) +
+      "\n Escolha uma opção digitando seu número:\n 1) Novo paciente\n 2) Consultar paciente\n 3) Sair"
+  );
+
+  switch (opções) {
+    case "1":
+      let novo = prompt("Nome do paciente a ser adicionado na fila:");
+      pacientes.push(novo);
+      break;
+    case "2":
+      alert("Paciente chamado para consulta: " + pacientes[0]);
+      pacientes.shift();
+      break;
+    case "3":
+      alert("Saindo...");
+      break;
+    default:
+      alert("Opção inválida");
+  }
+} while (opções !== "3");
