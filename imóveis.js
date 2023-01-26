177c3f5a03eadc1842f14d14b560f5619c7be800
LacerdaKris <107269748+LacerdaKris@users.noreply.github.com>
2023-01-26 16:58:31 -0300

    Update imóveis.js

--- imóveis.js
+++ imóveis.js
@@ -1,23 +1,24 @@
-let imóveis = {};
-total = 0;
+let imóveis = [];
 let opção = "";
 
 do {
   opção = prompt(
     "Imóveis cadastrados: " +
-      total +
+      imóveis.lenght +
       "\n" +
       "Digite:\n1) Para cadastrar um imóvel\n2) Mostrar todos os imóveis\n3) Sair"
   );
 
   switch (opção) {
     case "1":
-      let proprietario = prompt("Nome do proprietário: ");
-      let quartos = prompt("Quantidade de quartos: ");
-      let banheiros = prompt("Quantidade de banheiros: ");
-      let garagem = prompt("Possui garagem? (sim/não) ");
-      total += 1;
-      imóveis.imóvel({
+      const imovel = {}
+
+      imovel.proprietario = prompt("Nome do proprietário: ");
+      imovel.quartos = prompt("Quantidade de quartos: ");
+      imovel.banheiros = prompt("Quantidade de banheiros: ");
+      imovel.garagem = prompt("Possui garagem? (sim/não) ");
+            }
+      imóveis.unshift({
         "Nome do proprietário: ": proprietario,
         "Quartos: ": quartos,
         "Banheiros: ": banheiros,
@@ -25,7 +26,7 @@ do {
       });
       break;
     case "2":
-      alert(imóveis);
+      alert(imóveis.join((separador = "\n")));
       break;
     case "3":
       alert("Saindo...");
